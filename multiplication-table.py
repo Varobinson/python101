b8ff93da671b7b9dbb0608545acb690eedae52bf
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2020-08-20 18:44:18 -0400

    final update to multiplyer

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,9 +1,8 @@
-# initiate our outer loop counter as 1
 x = 0
 y = 0
-# we want the outer loop to loop 10 times so we could say <= 10 or < 11
-while x <= 9:
-    x = x + 1
+
+while x < 10:
+    x += 1
     y = 0
     while y<= 10:
         print(f'{x} X {y} = {x*y}')
