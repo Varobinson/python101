911d38d35a0757b978e12e672e71d838d64272e9
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2020-08-20 12:05:54 -0400

    Print 0 to 100

---
+++ print-0-thru-99.py
@@ -0,0 +1,12 @@
+#1. Print the numbers from 0 to 99
+for number in range(100):
+    print(number)
+
+
+#1. Print the even numbers from 0 to 99
+for number in range(0,100,2):
+    print(number)
+
+#1. Print the even numbers from 0 to 99
+for number in range(1, 100, 2):
+    print(number)
